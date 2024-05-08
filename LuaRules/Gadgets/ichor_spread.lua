1948a525db9ca076a1d1ed05ffa4eb42f482b75e
IcarusRTS <164705095+IcarusRTS@users.noreply.github.com>
2024-05-08 20:20:11 +0000

    added modinfo.lua and ichor_spread.lua

---
+++ LuaRules/Gadgets/ichor_spread.lua
@@ -0,0 +1,12 @@
+function gadget:GetInfo()
+	return {
+		name      = "Ichor Spread",
+		desc      = "",
+		author    = "IcarusRTS",
+		date      = "",
+		license   = "GNU GPL, v2 or later",
+		layer     = 0,
+		enabled   = true  --  loaded by default?
+	}
+end
+
---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name='Ichor Evolution',
+	description='Zero-K',
+	version='v0.1.0',
+	shortname='ZKIE',
+	mutator='1',
+	game='Zero-K',
+	shortGame='ZK',
+	modtype=1,
+	depend = {
+		[[rapid://zk:stable]],
+	},
+}
