b9e6d73da9acd5f98940ee104f3dad4c942f00cf
IcarusRTS <164705095+IcarusRTS@users.noreply.github.com>
2024-05-10 15:35:20 +0000

    removed unused cooldown parameter

--- LuaRules/Configs/ichor_spread_defs.lua
+++ LuaRules/Configs/ichor_spread_defs.lua
@@ -5,7 +5,6 @@ for i=1, #UnitDefs do
 	local cp = UnitDefs[i].customParams
 	if cp.ichor_spread then
 		unitArray[i] = {
-			cooldown = tonumber (cp.ichor_spread_cooldown),
 			spawns = cp.ichor_spread_spawn,
 		}
 	end
