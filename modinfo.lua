9cdf9284414b5960042c41ddb24982ca1c4e4248
IcarusRTS <164705095+IcarusRTS@users.noreply.github.com>
2024-07-20 01:11:59 +0000

    Create a new spring game

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+return {
+	name = 'Ichor Evolution',
+	description = 'Ichor Evolution',
+	shortname = 'ichorevo',
+	version = 'v1',
+	mutator = '1',
+	game = 'ichorevo',
+	shortGame = 'IE',
+	modtype=1,
+	depend = {
+	},
+}
